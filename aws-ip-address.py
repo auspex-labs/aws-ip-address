51cd231a802c67c40827567ab68d641dd3d501d6
Jason E. Robinson <jason.robinson@practicalcode.us>
2020-07-18 01:56:40 -0400

    Process IPv6 networks
    Signed-off-by: Jason E. Robinson <jason.robinson@practicalcode.us>

--- aws-ip-address.py
+++ aws-ip-address.py
@@ -17,16 +17,18 @@ for prefix in ip_ranges_json["prefixes"]:
 
 ipv6prefixes = list()
 for prefix in ip_ranges_json["ipv6_prefixes"]:
-    ipv6prefixes.append(prefix["ipv6_prefix"])
+    ipv6prefixes.append((ipaddress.ip_network(prefix["ipv6_prefix"]).exploded))
 
 # Remove any duplicate CIDRs
 
 ipv4cidr = list()
 [ipv4cidr.append(x) for x in ipv4prefixes if x not in ipv4cidr]
+ipv4cidr.sort()
 
 
 ipv6cidr = list()
 [ipv6cidr.append(x) for x in ipv6prefixes if x not in ipv6cidr]
+ipv6cidr.sort()
 
 # Remove CIDRs that are subnets of other CIDRs.
 
@@ -36,22 +38,26 @@ for cidr in ipv4cidr.copy():
             if ipaddress.ip_network(cidr).subnet_of(ipaddress.ip_network(net)):
                 ipv4cidr.remove(cidr)
 
-# for cidr in ipv6cidr.copy():
-#    for net in ipv6cidr.copy():
-#        if cidr != net:
-#            if ipaddress.ip_network(cidr).subnet_of(ipaddress.ip_network(net)):
-#                    ipv6cidr.remove(cidr)
+for cidr in ipv6cidr.copy():
+    for net in ipv6cidr.copy():
+        if cidr != net:
+            if ipaddress.ip_network(cidr).subnet_of(ipaddress.ip_network(net)):
+                ipv6cidr.remove(cidr)
 
-
-# Find adjacent CIDRs and merge them if possible.
+# Find and merge adjacent CIDRs.
 
 ipv4networks = dict()
 [ipv4networks.update({(int(x.split("/")[1])): []}) for x in ipv4cidr if int(x.split("/")[1]) not in ipv4networks.keys()]
 
+for cidr in ipv4cidr:
+    ipv4networks[int(cidr.split("/")[1])].append(str(cidr.split("/")[0]))
+
+
 ipv6networks = dict()
 [ipv6networks.update({(int(x.split("/")[1])): []}) for x in ipv6cidr if int(x.split("/")[1]) not in ipv6networks.keys()]
 
+for cidr in ipv6cidr:
+    ipv6networks[int(cidr.split("/")[1])].append(str(cidr.split("/")[0]))
 
-print(ipv4networks)
 
 #  Return a list of CIDRs
