be03e2f74ba250a82967e05240d7d9d1b1f2648f
Jason E. Robinson <jason.robinson@practicalcode.us>
2020-07-17 21:19:30 -0400

    Initial commit

---
+++ aws-ip-address.py
@@ -0,0 +1,10 @@
+# Obtain the AWS IP address range from https://ip-ranges.amazonaws.com/ip-ranges.json
+
+# Extract the CIDRs from the JSON file.
+
+# Remove any duplicate CIDRs
+
+# Find adjacent CIDRs and merge them if possible.
+
+#  Return a list of CIDRs
+
