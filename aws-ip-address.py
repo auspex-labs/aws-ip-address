3308e457d5b87f39e722c43451ab8072d1f73e27
Jason E. Robinson <jason.robinson@practicalcode.us>
2020-07-17 21:28:20 -0400

    Download the AWS IP Address file

--- aws-ip-address.py
+++ aws-ip-address.py
@@ -1,4 +1,11 @@
+import requests
+import json
+
 # Obtain the AWS IP address range from https://ip-ranges.amazonaws.com/ip-ranges.json
+
+url = "https://ip-ranges.amazonaws.com/ip-ranges.json"
+
+ip_ranges_json = requests.get(url).content
 
 # Extract the CIDRs from the JSON file.
 
