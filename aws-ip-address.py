f10f6a557a6e6a9f5f3885dc8d907e677a59e215
Jason E. Robinson <jason.robinson@practicalcode.us>
2020-07-18 06:01:42 -0400

    Made the code functional

--- aws-ip-address.py
+++ aws-ip-address.py
@@ -3,85 +3,100 @@ import json
 
 from ipaddress import ip_network
 
+#TODO Utilize Pandas
+
 # Obtain the AWS IP address range from https://ip-ranges.amazonaws.com/ip-ranges.json
 
-url = "https://ip-ranges.amazonaws.com/ip-ranges.json"
+def download_addresses(url="https://ip-ranges.amazonaws.com/ip-ranges.json"):
 
-ip_ranges_json = json.loads(requests.get(url).content)
+    ip_ranges_json = json.loads(requests.get(url).content)
+
+    return ip_ranges_json
 
 # Extract the CIDRs from the JSON file.
 
-ipv4prefixes = list()
-for prefix in ip_ranges_json["prefixes"]:
-    ipv4prefixes.append(prefix["ip_prefix"])
+def extract_ipv4(ip_ranges_json):
 
-ipv6prefixes = list()
-for prefix in ip_ranges_json["ipv6_prefixes"]:
-    ipv6prefixes.append((ip_network(prefix["ipv6_prefix"]).exploded))
+    ipv4prefixes = list()
+    for prefix in ip_ranges_json["prefixes"]:
+        ipv4prefixes.append(prefix["ip_prefix"])
 
-# Remove any duplicate CIDRs
+    return ipv4prefixes
 
-ipv4cidr = list()
-[ipv4cidr.append(x) for x in ipv4prefixes if x not in ipv4cidr]
-ipv4cidr.sort()
+def extract_ipv6(ip_ranges_json):
 
-ipv6cidr = list()
-[ipv6cidr.append(x) for x in ipv6prefixes if x not in ipv6cidr]
-ipv6cidr.sort()
+    ipv6prefixes = list()
+    for prefix in ip_ranges_json["ipv6_prefixes"]:
+        ipv6prefixes.append((ip_network(prefix["ipv6_prefix"]).exploded))
 
-# Remove CIDRs that are subnets of other CIDRs.
+    return ipv6prefixes
 
-for cidr in ipv4cidr.copy():
-    for net in ipv4cidr.copy():
-        if cidr != net:
-            if ip_network(cidr).subnet_of(ip_network(net)):
-                ipv4cidr.remove(cidr)
+def reduce_cidr(prefixes):
 
-for cidr in ipv6cidr.copy():
-    for net in ipv6cidr.copy():
-        if cidr != net:
-            if ip_network(cidr).subnet_of(ip_network(net)):
-                ipv6cidr.remove(cidr)
+    # Remove any duplicate CIDRs
 
-# Find and merge adjacent CIDRs.
+    cidr = list()
+    [cidr.append(x) for x in prefixes if x not in cidr]
+    cidr.sort()
 
-ipv4networks = dict()
-[ipv4networks.update({(int(x.split("/")[1])): []}) for x in ipv4cidr if int(x.split("/")[1]) not in ipv4networks.keys()]
+    # Remove CIDRs that are subnets of other CIDRs.
 
-for cidr in ipv4cidr:
-    ipv4networks[int(cidr.split("/")[1])].append(cidr)
+    for net in cidr.copy():
+        for sub in cidr.copy():
+            if net != sub:
+                if ip_network(net).subnet_of(ip_network(sub)):
+                    cidr.remove(net)
 
-updates = True
-while updates:
-    updates = False
-    for mask in sorted(ipv4networks.copy()):
-        for network in ipv4networks[mask].copy():
-            complete = True
-            for sub in ip_network(network).supernet().subnets():
-                if str(sub) not in ipv4networks[mask]:
-                    complete = False
-            if complete:
-                updates = True
-                supernet = str(ip_network(network).supernet())
-                if int(supernet.split("/")[1]) in ipv4networks:
-                    ipv4networks[int(supernet.split("/")[1])].append(supernet)
-                else:
-                    ipv4networks.update({(int(supernet.split("/")[1])): [supernet]})
+    # Find and merge adjacent CIDRs.
+
+    networks = dict()
+    [networks.update({(int(x.split("/")[1])): []}) for x in cidr if int(x.split("/")[1]) not in networks.keys()]
+
+    for net in cidr:
+        networks[int(net.split("/")[1])].append(net)
+
+    updates = True
+    while updates:
+        updates = False
+        for mask in sorted(networks.copy()):
+            for network in networks[mask].copy():
+                complete = True
                 for sub in ip_network(network).supernet().subnets():
-                    ipv4networks[mask].remove(str(sub))
+                    if str(sub) not in networks[mask]:
+                        complete = False
+                if complete:
+                    updates = True
+                    supernet = str(ip_network(network).supernet())
+                    if int(supernet.split("/")[1]) in networks:
+                        networks[int(supernet.split("/")[1])].append(supernet)
+                    else:
+                        networks.update({(int(supernet.split("/")[1])): [supernet]})
+                    for sub in ip_network(network).supernet().subnets():
+                        networks[mask].remove(str(sub))
 
-count = 0
-for mask in ipv4networks:
-    count += len(ipv4networks[mask])
-
-ipv6networks = dict()
-[ipv6networks.update({(int(x.split("/")[1])): []}) for x in ipv6cidr if int(x.split("/")[1]) not in ipv6networks.keys()]
-
-for cidr in ipv6cidr:
-    ipv6networks[int(cidr.split("/")[1])].append(cidr)
+    return networks
 
 #  Return a list of CIDRs
 
-for mask in sorted(ipv4networks):
-    for cidr in sorted(ipv4networks[mask]):
-        print(cidr)
+def print_cidr(networks):
+
+    for mask in sorted(networks):
+        for cidr in sorted(networks[mask]):
+            print(cidr)
+
+    return
+
+
+ip_ranges_json = download_addresses()
+
+ipv4 = extract_ipv4(ip_ranges_json)
+
+ipv6 = extract_ipv6(ip_ranges_json)
+
+ipv4 = reduce_cidr(ipv4)
+
+ipv6 = reduce_cidr(ipv6)
+
+print_cidr(ipv4)
+
+print_cidr(ipv6)
